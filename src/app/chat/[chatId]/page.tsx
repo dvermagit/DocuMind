a25a64e1c5e34bc5f1d45bd83c542f6ac055d78c
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-06-06 18:08:11 +0530

    pdf viewer component

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,13 +1,16 @@
 import ChatSidebar from "@/components/ChatSidebar";
+import PDFViewer from "@/components/PDFViewer";
 import { db } from "@/lib/db";
 import { chats } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 
-interface ChatPageProps {
-  params: { chatId: string };
-}
+type ChatPageProps = {
+  params: {
+    chatId: string;
+  };
+};
 export default async function ChatPage({ params: { chatId } }: ChatPageProps) {
   const { userId } = await auth();
   if (!userId) {
@@ -20,6 +23,8 @@ export default async function ChatPage({ params: { chatId } }: ChatPageProps) {
   if (!_chats.find((chat) => chat.id === parseInt(chatId))) {
     return redirect("/");
   }
+
+  const currentChat = _chats.find((chats) => chats.id === parseInt(chatId));
   return (
     <div className="flex max-h-screen overflow-scroll">
       <div className="flex w-full max-h-screen overflow-scroll">
@@ -30,7 +35,7 @@ export default async function ChatPage({ params: { chatId } }: ChatPageProps) {
 
         {/* pdf viewer */}
         <div className="max-h-screen p-4 overflow-scroll flex-[5]">
-          {/* <PDFViewer/> */}
+          <PDFViewer pdf_url={currentChat?.pdfUrl || ""} />
         </div>
 
         {/* chat component */}
