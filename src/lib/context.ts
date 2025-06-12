6c82da10d89fbb23dfe3554b3393e6cd7a0c9a7a
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-06-12 18:41:56 +0530

    pinecone openAi get context

---
+++ src/lib/context.ts
@@ -0,0 +1,59 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { convertToAscii } from "./utils";
+import { getEmbedding } from "./embeddings";
+
+export async function getMatchesFromEmbeddings(
+  embeddings: number[],
+  fileKey: string
+) {
+  const pinecone = new Pinecone({
+    apiKey: process.env.NEXT_PUBLIC_PINECONE_API_KEY!,
+  });
+
+  const index = await pinecone.index("documind-ai");
+
+  try {
+    // const namespace = convertToAscii(fileKey);
+    // const queryResult = await index.query({
+    //   topK: 5,
+    //   vector: embeddings,  // Correct property name
+    //   includeMetadata: true,
+    //   namespace
+    // });
+    // return queryResult.matches || [];
+    const namespace = convertToAscii(fileKey);
+    const queryResult = await index.namespace("default").query({
+      topK: 5,
+      vector: embeddings,
+      includeMetadata: true,
+    });
+    return queryResult.matches || [];
+  } catch (error) {
+    console.log("error querying embeddings", error);
+    throw error;
+  }
+}
+
+export async function getContext(query: string, fileKey: string) {
+  const queryEmbeddings = await getEmbedding(query);
+  console.log({ queryEmbeddings });
+  const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+
+  const qualifyingDocs = matches.filter(
+    (matches) => matches.score && matches.score > 0.5
+  );
+
+  console.log({ qualifyingDocs });
+  type Metadata = {
+    text: string;
+    pageNumber: number;
+  };
+
+  let docs = qualifyingDocs.map((match) => {
+    console.log(match.metadata);
+    return (match.metadata as Metadata).text;
+  });
+
+  console.log({ docs });
+  return docs.join("\n").substring(0, 3000);
+}
