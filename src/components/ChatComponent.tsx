b45758c88ff5fa894684ccedfa2c1537bd6579a8
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-06-12 18:40:03 +0530

    chat component

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -4,13 +4,34 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { useChat } from "@ai-sdk/react";
 import { MessagesList } from "./MessagesList";
-export const ChatComponent = () => {
+import React from "react";
+
+type Props = {
+  chatId: number;
+};
+export const ChatComponent = ({ chatId }: Props) => {
   const { input, handleInputChange, handleSubmit, messages } = useChat({
     api: "/api/chat",
+    // id: chatId.toString(),
+    body: {
+      chatId: chatId.toString(),
+    },
   });
 
+  React.useEffect(() => {
+    const messageContainer = document.getElementById("message-container");
+    if (messageContainer) {
+      messageContainer.scrollTo({
+        top: messageContainer.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
   return (
-    <div className="relative max-h-screen overflow-scroll">
+    <div
+      className="relative max-h-screen overflow-scroll"
+      id="message-container"
+    >
       {/* header */}
       <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
         <h3 className="text-xl font-bold">Chat</h3>
@@ -21,7 +42,7 @@ export const ChatComponent = () => {
 
       <form
         onSubmit={handleSubmit}
-        className="sticky ottom-0 inset-x-0 px-2 py-4 bg-white"
+        className="sticky bottom-0 inset-x-0 px-2 py-4 bg-white"
       >
         <div className="flex">
           <Input
